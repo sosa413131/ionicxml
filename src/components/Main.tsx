91d696c7579c935ebec9dd05cd2b2179be6df34c
David W Sosa <sosa.david88@gmail.com>
2022-04-22 17:07:20 -0400

    iterated over xml and added working anchor tags

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,11 +8,12 @@ import './Main.css';
 interface ContainerProps { }
 
 const Main: React.FC<ContainerProps> = () => {    
-  const [postArray, setPostArray] = useState<any>([]);
+  const [postArray, setPostArray] = useState<any>(null);
 
   useEffect(() => {
     // update the state with post info
-    setPostArray(fetchPostArray());
+    fetchPostArray();
+    // console.log("postArray:", postArray)
   }, []);
 
 function fetchPostArray():any {
@@ -21,23 +22,31 @@ function fetchPostArray():any {
 
     const xml=response.data;
     const postArray = txml.parse(response.data)[1]['children']
-    console.log(postArray)
-    return postArray;
+    console.log(postArray[0])
+    setPostArray(postArray);
 
    }).catch(error=> console.log(`Error: ${error}`));
   }
 
 if(postArray){
   return (
-    // <div className='blogPosts'>
-    //      {postArray.map(post=>{
-    //        return (
-    //        <div> 
-    //          <Post title={post[2]['children'][0]} link='' author='' summary='' id='' imageUrl='' date='' />
-    //          </div>)
-    //      })}
-    // </div>
-    <h1>test</h1>
+    <div className='blogPosts'>
+         {postArray.map((post:any, index:string)=>{
+           return (
+           <div key={index}> 
+             <Post 
+             title={post['children'][2]['children'][0]} 
+             link={`/summary/${post['attributes']['id']}`} 
+             author='' 
+             summary='' 
+             id='' 
+             imageUrl='' 
+             date='' />
+             </div>)
+         })}
+    </div>
+
+    // <>test</>
   );
 }else{
   return <>No Posts</>
